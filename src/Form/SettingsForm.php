1582581c79c9928a35436495a5906af377a08aa8
Dave Bailey <david.bailey@autodesk.com>
2021-01-21 15:24:30 -0700

    Issue #3154796 by kavitasonawane, dimr: Remove @file tag docblock from all the .php files. & $this->t() should be used instead of t() for Drupal 8 version

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\ape\Form\SettingsForm
- */
-
 namespace Drupal\ape\Form;
 
 use Drupal\Component\Plugin\Factory\FactoryInterface;
@@ -95,75 +90,75 @@ class SettingsForm extends ConfigFormBase {
 
     $form['page_caching'] = array(
       '#type' => 'fieldset',
-      '#title' => t('General page caching'),
+      '#title' => $this->t('General page caching'),
       '#collapsible' => FALSE,
       '#collapsed' => FALSE,
     );
 
     $period = [0, 60, 180, 300, 600, 900, 1800, 2700, 3600, 10800, 21600, 32400, 43200, 86400, 604800, 2592000, 31536000];
     $period = array_map(array($this->dateFormatter, 'formatInterval'), array_combine($period, $period));
-    $period[0] = '<' . t('no caching') . '>';
+    $period[0] = '<' . $this->t('no caching') . '>';
 
     $form['page_caching']['page_cache_maximum_age'] = array(
       '#type' => 'select',
-      '#title' => t('Global page expiration'),
+      '#title' => $this->t('Global page expiration'),
       '#options' => $period,
       '#default_value' => $config_system->get('cache.page.max_age'),
-      '#description' => t('The standard expiration lifetime for cached pages. Ideally this is set as long as possible.'),
+      '#description' => $this->t('The standard expiration lifetime for cached pages. Ideally this is set as long as possible.'),
     );
 
     // Pages visibility plugin.
     $this->excluded->setConfig('pages', $config_ape->get('exclusions'));
     $form['page_caching'] += $this->excluded->buildConfigurationForm([], $form_state);
     unset($form['page_caching']['negate']);
-    $form['page_caching']['pages']['#title'] = t('Pages to exclude from caching');
+    $form['page_caching']['pages']['#title'] = $this->t('Pages to exclude from caching');
 
     $form['page_caching_alternative'] = array(
       '#type' => 'fieldset',
-      '#title' => t('Alternative page caching'),
+      '#title' => $this->t('Alternative page caching'),
       '#collapsible' => FALSE,
       '#collapsed' => FALSE,
     );
     $form['page_caching_alternative']['ape_alternative_lifetime'] = array(
       '#type' => 'select',
-      '#title' => t('Alternative page expiration'),
+      '#title' => $this->t('Alternative page expiration'),
       '#options' => $period,
       '#default_value' => $config_ape->get('lifetime.alternatives'),
-      '#description' => t('An alternative page expiration lifetime. Useful for pages that should refresh at a different rate than most pages, such as a short interval like 5 minutes.'),
+      '#description' => $this->t('An alternative page expiration lifetime. Useful for pages that should refresh at a different rate than most pages, such as a short interval like 5 minutes.'),
     );
 
     // Pages visibility plugin.
     $this->alternatives->setConfig('pages', $config_ape->get('alternatives'));
     $form['page_caching_alternative'] += $this->alternatives->buildConfigurationForm([], $form_state);
     unset($form['page_caching_alternative']['negate']);
-    $form['page_caching_alternative']['pages']['#title'] = t('Pages that should apply alternative cache length');
+    $form['page_caching_alternative']['pages']['#title'] = $this->t('Pages that should apply alternative cache length');
 
     $form['server_codes'] = array(
       '#type' => 'fieldset',
-      '#title' => t('Server response caching'),
+      '#title' => $this->t('Server response caching'),
       '#collapsible' => FALSE,
       '#collapsed' => FALSE,
     );
     $form['server_codes']['ape_301_lifetime'] = array(
       '#type' => 'select',
-      '#title' => t('301 Redirects Expiration'),
+      '#title' => $this->t('301 Redirects Expiration'),
       '#options' => $period,
       '#default_value' => $config_ape->get('lifetime.301'),
-      '#description' => t('Set a cache lifetime for all 301 redirects.'),
+      '#description' => $this->t('Set a cache lifetime for all 301 redirects.'),
     );
     $form['server_codes']['ape_302_lifetime'] = array(
       '#type' => 'select',
-      '#title' => t('302 Redirects Expiration'),
+      '#title' => $this->t('302 Redirects Expiration'),
       '#options' => $period,
       '#default_value' => $config_ape->get('lifetime.302'),
-      '#description' => t('Set a cache lifetime for all 302 redirects.'),
+      '#description' => $this->t('Set a cache lifetime for all 302 redirects.'),
     );
     $form['server_codes']['ape_404_lifetime'] = array(
       '#type' => 'select',
-      '#title' => t('404 Page Not Found Expiration'),
+      '#title' => $this->t('404 Page Not Found Expiration'),
       '#options' => $period,
       '#default_value' => $config_ape->get('lifetime.404'),
-      '#description' => t('Set a cache lifetime for all 404 Page Not Found responses.'),
+      '#description' => $this->t('Set a cache lifetime for all 404 Page Not Found responses.'),
     );
 
     return parent::buildForm($form, $form_state);
